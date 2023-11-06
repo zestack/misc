6dc59f9046f2540b96a4114380c583ecf1bc1a06
zestack <simplism.dev@gmail.com>
2023-11-06 09:27:17 +0800

    feat: 简单实现 weakmap，当 goroutine 结束时自动回收关联数据

---
+++ weakmap.go
@@ -0,0 +1,61 @@
+package misc
+
+import (
+	"reflect"
+	"runtime"
+	"sync"
+)
+
+type WeakMap struct {
+	m map[uintptr]any
+	l sync.RWMutex
+}
+
+func NewWeakMap() *WeakMap {
+	return &WeakMap{
+		m: make(map[uintptr]any),
+	}
+}
+
+// Put key is weakly referenced.
+func (w *WeakMap) Put(key, value any) {
+	runtime.SetFinalizer(key, w.finalizer)
+	ptr := reflect.ValueOf(key).Pointer()
+	w.l.Lock()
+	defer w.l.Unlock()
+	w.m[ptr] = value
+}
+
+func (w *WeakMap) Get(key any) (any, bool) {
+	ptr := reflect.ValueOf(key).Pointer()
+	w.l.RLock()
+	defer w.l.RUnlock()
+	v, ok := w.m[ptr]
+	return v, ok
+}
+
+func (w *WeakMap) Has(key any) bool {
+	ptr := reflect.ValueOf(key).Pointer()
+	w.l.RLock()
+	defer w.l.RUnlock()
+	_, ok := w.m[ptr]
+	return ok
+}
+
+func (w *WeakMap) Delete(key any) {
+	w.finalizer(key)
+}
+
+func (w *WeakMap) Len() int {
+	w.l.RLock()
+	defer w.l.RUnlock()
+	v := len(w.m)
+	return v
+}
+
+func (w *WeakMap) finalizer(key any) {
+	ptr := reflect.ValueOf(key).Pointer() // uintptr(unsafe.Pointer(&key))
+	w.l.Lock()
+	delete(w.m, ptr)
+	w.l.Unlock()
+}
